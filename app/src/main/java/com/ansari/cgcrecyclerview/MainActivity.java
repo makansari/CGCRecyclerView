6af5c6de1ba31273d35affa6f532ae48fa2db0d4
makansari78 <mak.ansari@rjtcompuquest.com>
2019-01-18 15:39:14 +0530

    webservices

--- app/src/main/java/com/ansari/cgcrecyclerview/MainActivity.java
+++ app/src/main/java/com/ansari/cgcrecyclerview/MainActivity.java
@@ -1,9 +1,21 @@
 package com.ansari.cgcrecyclerview;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     List<Movie> movies;
     RecyclerView recyclerView;
     MyAdapter myAdapter;
+    ProgressDialog progressDialog;
+    Movie myMovie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,14 +34,67 @@ public class MainActivity extends AppCompatActivity {
         movies = new ArrayList<>();
         recyclerView = findViewById(R.id.myRecyclerview);
 
-        myAdapter = new MyAdapter(movies);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-        recyclerView.setAdapter(myAdapter);
+
+        getDataFromServer();
+
+        //addData();
+    }
 
 
 
-        addData();
+    private void getDataFromServer() {
+
+        String url = "https://api.androidhive.info/contacts/";
+
+        progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setMessage("Downloading data from server !!!!");
+        progressDialog.show();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+
+             @Override
+            public void onResponse(String response) {
+
+                progressDialog.cancel();
+                try {
+                    JSONObject jsonObj = new JSONObject(response);
+                    JSONArray contancts = jsonObj.getJSONArray("contacts");
+
+                    for(int i=0; i<contancts.length();i++){
+                        JSONObject data = contancts.getJSONObject(i);
+                        String name = data.getString("name");
+                        String email = data.getString("email");
+                        String gender = data.getString("gender");
+
+                        myMovie = new Movie(name,email,gender);
+                        movies.add(myMovie);
+
+                    }
+                    myAdapter = new MyAdapter(movies);
+                    recyclerView.setAdapter(myAdapter);
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+
+
+        RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+        requestQueue.add(stringRequest);
+
+
+
+
     }
 
     private void addData() {
